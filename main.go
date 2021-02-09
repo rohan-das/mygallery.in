d4f44cd511b2b12d9617198dcd16a439cb8e8e30
Rohan Das <hello.rd24@gmail.com>
2021-02-09 08:58:56 +0530

    Added 404 status code

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprintf(w, "To get in touch, please contact our support at <a href=\"mailto:support@mygallery.in\">support@mygallery.in</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "<h1>Sorry, you reached an invalid page :(</h1>")
 	}
 }
 
