21507b7ba44340467ca2162deaf3a1faef6b57ce
Rohan Das <hello.rd24@gmail.com>
2021-02-09 08:34:58 +0530

    Added a contact page

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprintf(w, "To get in touch, please contact our support at <a href=\"mailto:support@mygallery.in\">support@mygallery.in</a>.")
+	}
 }
 
 func main() {
