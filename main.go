c86fcdad20af9c15ecff5f5e5ce8097c73f45c77
Rohan Das <hello.rd24@gmail.com>
2021-02-21 11:44:42 +0530

    Created a simple contact template view

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"text/template"
 
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,7 +21,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "To get in touch, please contact our support at <a href=\"mailto:support@mygallery.in\">support@mygallery.in</a>.")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -28,8 +32,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
-	r.HandleFunc("/home", home)
+	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":8000", r)
 }
