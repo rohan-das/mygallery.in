c53dd2ffdc278949ab045aaa3739148b166dea91
Rohan Das <hello.rd24@gmail.com>
2021-03-06 13:16:50 +0530

    Created the Create() method on the users controller and updated our routes to have specific http verbs

--- controllers/user.go
+++ controllers/user.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"mygallery.in/views"
@@ -21,9 +22,20 @@ type Users struct {
 	NewView *views.View
 }
 
-// New method - Used as handler.
+// New is used to render the form where a user can
+// create a new user account.
+//
+// GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+// Create is used to process the signup form when a user
+// submits it. This is used to create a new account.
+//
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is a temporary response.")
 }
